02ed6847b3a4952e67c6df84a2a58eb2e7aba68e
eshaadreesofficial-lang <esha.adrees.official@gmail.com>
2026-07-26 19:12:54 +0500

    Update session2-class-code.py

--- Python-Fundamentals/Week-01-Session-02/session2-class-code.py
+++ Python-Fundamentals/Week-01-Session-02/session2-class-code.py
@@ -237,9 +237,9 @@ else:
 for i in {1,2,3,4,5}:
   print(i)
 
-"# For loop 
+# For loop 
 
-"# Program - The current population of a town is 10000. The population of the town is increasing at the rate of 10% per year. You have to write a program to find out the population at the end of each of the last 10 years.
+# Program - The current population of a town is 10000. The population of the town is increasing at the rate of 10% per year. You have to write a program to find out the population at the end of each of the last 10 years.
 
 curr_pop = 10000
 
